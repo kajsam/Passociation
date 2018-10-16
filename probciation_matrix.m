a8b4763e3d24a86ff8eb790b4d07ce5f2542c4d6
Kajsa Møllersen <kajsa.mollersen@uit.no>
2018-10-16 07:27:31 -0400

    Incorporate likelihood
    Doesn't not give the wanted results. Don't know why.

---
+++ probciation_matrix.m
@@ -0,0 +1,40 @@
+function Z = probciation_matrix(X, A, alphabeta, fig_nr)
+
+% Kajsa Mollersen (kajsa.mollersen@uit.no) October 15th 2018
+
+% Input:    X (n x d)           - binary gene expression matrix
+%           A (n x d)           - binary structure matrix approximation
+%           alphabeta           - the Bernoulli pi parameters
+
+% This function calculates n column vectors of length n, that will be
+% candidates column vectors for the next rank K approximation A of X.
+
+n = size(X,1);
+
+if islogical(X) && islogical(A)
+  if ~all(size(X)==size(A))
+    disp('Wrong dimensions')
+    return
+  end
+else
+  disp('Logical, please')
+  return
+end
+
+alpha = alphabeta(1,:);
+beta = alphabeta(2,:);
+
+% The probability/likelihood matrix
+Z = zeros(n,n);
+for i = 1: n
+  for ii = 1: n  % Compare column i in X to column ii in Pi
+    n00 = sum(~X(i,:) & ~ A(ii,:));
+    n01 =sum(~X(i,:) & A(ii,:));
+    n10 = sum(X(i,:) & ~A(ii,:));
+    n11 = sum(X(i,:) & A(ii,:));
+    
+    Z(ii,i) = (n10+n11)*alpha(ii) + n11*beta(ii) ...
+        - (n00+n10)*log(1 + exp(alpha(ii))) ...
+        - (n01 + n11)*log(1 + exp(alpha(ii) + beta(ii)));
+  end
+end
