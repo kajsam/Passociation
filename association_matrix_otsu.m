92d09971f426df9328cb2042ed7679a7172286dd
Kajsa Møllersen <kajsa.mollersen@uit.no>
2018-10-25 11:15:59 -0400

    Deletes small and big columns

--- association_matrix_otsu.m
+++ association_matrix_otsu.m
@@ -1,4 +1,4 @@
-function [binZ, thresh] = association_matrix_otsu(X, min_class, fig_nr)
+function [binZ, thresh] = association_matrix_otsu(X, min_class, max_class, fig_nr)
 
 % Input:    X - binary gene expression matrix
 
@@ -12,14 +12,16 @@ end
 % The association matrix
 Z = zeros(n,n);
 XnX = zeros(1,n);
-for i = 1: n                        % For each row, compare to all other rows 
+sumX = sum(X,2);
+sumX(sumX==0) = 1;
+for i = 1: n  
+  % For each row, compare to all other rows 
   for l = 1 :n
     XnX(l) = sum(X(l,:)& X(i,:));      % Which entries are both 1
   end           
-  sumX = sum(X(i,:));
-  sumX = max(sumX,1);               % In case sum = 0
-  Z(:,i) = XnX./sumX;        % Sum up row-wise, normalised by the total number of 1's in the row 
-  i
+  % sumX = sum(X(i,:));
+  % sumX = max(sumX,1);               % In case sum = 0
+  Z(:,i) = XnX./sumX(i);        % Sum up row-wise, normalised by the total number of 1's in the row 
 end
 
 % Z(logical(eye(n))) = median(Z(:));
@@ -31,15 +33,13 @@ end
 % Calculate the threshold for binarization. The diagonal is excluded.  
 num_bins = 256; %floor(n/25);
 thresh = zeros(1,n);
-binZ = false(n,n);
+binZ = true(n,n);
 for i = 1: n
-  z = Z(i,:);
-  % figure(fig_nr+1), hist(z([1:i-1 i+1:n]),num_bin)
-  thresh(i) = otsu_thresh(Z(i,[1:i-1 i+1:n]),num_bins);
-  % title(thresh(i))
-  % pause
+  z = Z(i,[1:i-1 i+1:n]) - min(Z(i,[1:i-1 i+1:n]));
+  z = z./max(z);
+  thresh(i) = otsu_thresh(z,num_bins);
   z(z<thresh(i)) = 0;
-  binZ(i,:) = logical(z);
+  binZ(i,[1:i-1 i+1:n]) = logical(z);
 end
 
 binZ = unique(binZ', 'rows', 'stable'); % Finding replicates. 
@@ -49,6 +49,10 @@ sumZ = sum(binZ,1);
 
 idx0 = sumZ < min_class;    % Smaller than the smallest class size
 binZ(:,idx0) = [];          % Noise, delete it
+sumZ = sum(binZ,1);
+idx1 = sumZ > max_class;
+[sum(idx0) sum(idx1)]
+binZ(:,idx1) = [];          % Column of zeros
 
 if fig_nr  
   figure(fig_nr), subplot(1,2,2), imagesc(binZ) 
